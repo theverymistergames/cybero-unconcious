fcd44bcbe2f7abfa57dedc9e1ba239e9ea57ddee
v.marinchenko <v.marinchenko@siridar.com>
2022-07-28 11:30:03 +0300

    Remove MV FPS, refactor scripts, update unity-common

---
+++ Assets/Scripts/CyberoUnconcious/Editor/LightUpdateMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.SceneManagement;
+
+namespace MisterGames.CyberoUnconcious.Editor {
+
+    public static class LightUpdateMenu {
+
+        [MenuItem("MisterGames/Render shadow map for OnDemand lights")]
+        private static void UpdateLights() {
+            var gameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+
+            foreach (var gameObject in gameObjects) {
+                var lights = gameObject.GetComponentsInChildren<HDAdditionalLightData>();
+                if (lights.Length == 0) continue;
+
+                foreach (var light in lights) {
+                    if (light.shadowUpdateMode != ShadowUpdateMode.OnDemand) continue;
+
+                    light.RequestShadowMapRendering();
+                }
+            }
+        }
+    }
+
+}
