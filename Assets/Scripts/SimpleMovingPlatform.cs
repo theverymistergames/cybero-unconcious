f2be9f799522bec2ccbbb26cdd5678fd7eb3e8aa
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-16 10:10:54 +0300

    Mark some project scripts as obsolete

--- Assets/Scripts/SimpleMovingPlatform.cs
+++ Assets/Scripts/SimpleMovingPlatform.cs
@@ -1,84 +1,13 @@
 ﻿using System;
-using MisterGames.Tick.Core;
-using Tween;
 using UnityEngine;
 
 namespace DefaultNamespace {
 
+    [Obsolete("SimpleMovingPlatform is obsolete")]
     public class SimpleMovingPlatform : MonoBehaviour {
 
-        [SerializeField] private Vector3[] positions = new Vector3[2];
-        [SerializeField] private SimpleDoor[] doors;
-        [SerializeField] private int startPositionID = 0;
-        [SerializeField] private string transformTweenID = "Transform";
-        
-        private bool _inProgress;
-        private int _currentPositionID;
-        private int _positionsTotal = 0;
-        private TweenController _controller;
-        private TransformTween _tween;
-
-        private void Start() {
-            _positionsTotal = positions.Length;
-            _currentPositionID = startPositionID;
-            _controller = gameObject.GetComponent<TweenController>();
-            
-            _tween = _controller.GetTweenById(transformTweenID) as TransformTween;
-            
-            if (_tween == null) {
-                throw new NullReferenceException("No tween with ID " + transformTweenID);
-            }
-            
-            _tween.SetStartPosition(positions[startPositionID]);
-            
-            _controller.Rewind();
-            
-            _controller.OnFinished += OnControllerFinished;
-        }
-
-        private void OnControllerFinished() {
-            _inProgress = false;
-
-            if (doors[_currentPositionID]) {
-                doors[_currentPositionID].Open();
-            }
-        }
-
-        public void MoveToPositionById(int newPositionID) {
-            if (newPositionID >= _positionsTotal || newPositionID < 0 || _inProgress) return;
-
-            if (newPositionID == _currentPositionID) {
-                if (doors[_currentPositionID]) {
-                    doors[_currentPositionID].Open();
-                }
-                
-                return;
-            }
-            
-            _tween.SetStartPosition(positions[_currentPositionID]);
-            _tween.SetEndPosition(positions[newPositionID]);
-            
-            if (doors[_currentPositionID]) {
-                doors[_currentPositionID].Close();
-            }
-
-            _currentPositionID = newPositionID;
-            
-            _controller.Play();
-
-            _inProgress = true;
-        }
-
-        public void MoveForward() {
-            if (_currentPositionID >= _positionsTotal || _inProgress) return;
-
-            MoveToPositionById(_currentPositionID + 1);
-        }
-        
-        public void MoveBackward() {
-            if (_currentPositionID <= 0 || _inProgress) return;
-            
-            MoveToPositionById(_currentPositionID - 1);
+        private void Awake() {
+            Debug.LogError($"SimpleMovingPlatform is obsolete: gameObject {gameObject.name}.");
         }
     }
 
--- Assets/Scripts/Tween/TweenController.cs
+++ Assets/Scripts/Tween/TweenController.cs
@@ -1,135 +1,44 @@
 ﻿using System;
-using System.Threading;
-using MisterGames.Common.Attributes;
-using MisterGames.Tick.Core;
-using MisterGames.Tick.Jobs;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Tween
 {
     [Obsolete("Need to replace TweenController with MisterGames.Tweens.Core.TweenRunner")]
     public class TweenController : MonoBehaviour, ITweenController
     {
-        [SerializeField] private PlayerLoopStage _timeSourceStage = PlayerLoopStage.Update;
-
-        [SerializeField] private float globalDelay;
-        [SerializeField] private float globalDuration;
-        [SerializeField] private Vector2 globalSpeedRange = new Vector2(1, 1);
-        [SerializeField] private bool autoStart = true;
-        [SerializeField] private bool loop;
-        [SerializeField] private bool chain; //TODO
-        [SerializeField][SubclassSelector][SerializeReference] private Tween[] tweens;
-
         public event Action OnFinished = delegate { };
 
-        private Job _job;
-        
         private void Awake() {
-            foreach (var tween in tweens) {
-                tween.Init(gameObject, _timeSourceStage);
-            }
-        }
-
-        private void OnDestroy() {
-            _job.Dispose();
-        }
-
-        private void Start() {
-            Rewind();
-
-            if (autoStart) StartPlayAfterDelay(globalDelay);
-        }
-
-        private void StartPlayAfterDelay(float delay) {
-            _job.Dispose();
-
-            _job = JobSequence.Create(_timeSourceStage)
-                .Delay(delay)
-                .Action(Play)
-                .Push()
-                .Start();
+            Debug.LogError($"TweenController is obsolete: gameObject {gameObject.name}. " +
+                           $"Need to replace TweenController with MisterGames.Tweens.Core.TweenRunner");
         }
 
         public void Pause() {
-            _job.Stop();
-            
-            foreach (var tween in tweens) {
-                tween.Pause();
-            }
+            throw new NotImplementedException();
         }
 
         public void Stop() {
-            _job.Stop();
-            
-            foreach (var tween in tweens) {
-                tween.Stop();
-            }
+            throw new NotImplementedException();
         }
 
         public void Resume() {
-            _job.Start();
-            
-            foreach (var tween in tweens) {
-                if (tween.Paused) tween.Resume();
-            }
+            throw new NotImplementedException();
         }
 
         public void Rewind() {
-            foreach (var tween in tweens) {
-                tween.Rewind();
-            }
+            throw new NotImplementedException();
         }
 
         public void Wind() {
-            foreach (var tween in tweens) {
-                tween.Wind();
-            }
+            throw new NotImplementedException();
         }
 
         public void Play() {
-            var speed = Random.Range(globalSpeedRange.x, globalSpeedRange.y);
-            
-            foreach (var tween in tweens) {
-                tween.GlobalSpeedMultiplier = speed;
-                if (tween.IsAutoStart) tween.Play();
-            }
-
-            if (loop && globalDuration > 0) {
-                _job.Dispose();
-
-                _job = JobSequence.Create(_timeSourceStage)
-                    .Delay(globalDuration * speed)
-                    .Action(Rewind)
-                    .Action(Play)
-                    .Push()
-                    .Start();
-
-                return;
-            }
-
-            if (globalDuration > 0) {
-                _job.Dispose();
-
-                _job = JobSequence.Create(_timeSourceStage)
-                    .Delay(globalDuration * speed)
-                    .Action(() => {
-                        Stop();
-                        OnFinished.Invoke();
-                    })
-                    .Push()
-                    .Start();
-            }
+            throw new NotImplementedException();
         }
 
         public void Revert(bool reverted) {
-            foreach (var tween in tweens) {
-                tween.Reverted = reverted;
-            }
-        }
-
-        public Tween GetTweenById(string id) {
-            return Array.Find(tweens, tween => tween.ID == id);
+            throw new NotImplementedException();
         }
     }
 }
