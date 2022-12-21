b5462586fb10c4e534668b7de35093979bbe2006
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-21 13:01:40 +0300

    Update LightUpdateMenu.cs

--- Assets/Scripts/CyberoUnconcious/Editor/LightUpdateMenu.cs
+++ Assets/Scripts/CyberoUnconcious/Editor/LightUpdateMenu.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.SceneManagement;
 
@@ -6,10 +8,12 @@ namespace MisterGames.CyberoUnconcious.Editor {
 
     public static class LightUpdateMenu {
 
-        [MenuItem("MisterGames/Render shadow map for OnDemand lights")]
+        [MenuItem("MisterGames/Tools/Render shadow map for OnDemand lights %&L")]
         private static void UpdateLights() {
-            var gameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+            Debug.Log($"Requested shadow map rendering for light with ShadowUpdateMode {ShadowUpdateMode.OnDemand}");
+            int lightRequestsCount = 0;
 
+            var gameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
             foreach (var gameObject in gameObjects) {
                 var lights = gameObject.GetComponentsInChildren<HDAdditionalLightData>();
                 if (lights.Length == 0) continue;
@@ -18,8 +22,21 @@ namespace MisterGames.CyberoUnconcious.Editor {
                     if (light.shadowUpdateMode != ShadowUpdateMode.OnDemand) continue;
 
                     light.RequestShadowMapRendering();
+                    lightRequestsCount++;
+
+                    Debug.Log($"Requested shadow map rendering for light on game object '{light.gameObject}'");
+                    EditorUtility.SetDirty(light);
                 }
             }
+
+            if (lightRequestsCount == 0) {
+                Debug.Log($"No game objects on the current scene have lights with ShadowUpdateMode {ShadowUpdateMode.OnDemand}. " +
+                          $"Nothing to render.");
+                return;
+            }
+
+            Debug.Log($"Requested shadow map rendering for light with ShadowUpdateMode {ShadowUpdateMode.OnDemand} done. " +
+                      $"Rendered {lightRequestsCount} lights.");
         }
     }
 
