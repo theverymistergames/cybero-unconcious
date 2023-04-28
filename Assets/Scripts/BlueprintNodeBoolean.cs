37c71cea66c3285bed2ceab6ff850e75e227e445
fotik333 <nobelisfm@mail.ru>
2023-04-28 12:11:39 +0300

    upd

---
+++ Assets/Scripts/BlueprintNodeBoolean.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MisterGames.Blueprints;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Boolean", Category = "Logic", Color = BlueprintColors.Node.Data)]
+    public sealed class BlueprintNodeBoolean : BlueprintNode, IBlueprintOutput<bool> {
+
+        [SerializeField] private bool _value = true;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Output<bool>("Value"),
+        };
+
+        bool IBlueprintOutput<bool>.GetOutputPortValue(int port) => port switch {
+            0 => _value
+        };
+    }
+
+}
---
+++ Assets/Scripts/BlueprintNodeIterate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MisterGames.Blueprints;
+using MisterGames.Blueprints.Meta;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Iterate", Category = "GameObject", Color = BlueprintColors.Node.Data)]
+    public sealed class BlueprintNodeIterate : BlueprintNode
+    {
+        private bool _value = true;
+        
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter(),
+            Port.Input<Array>("Array"),
+        };
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            var gameObject = Ports[1].Get<GameObject>();
+            var value = Ports[2].Get(_value);
+
+            gameObject.SetActive(value);
+        }
+    }
+
+}
