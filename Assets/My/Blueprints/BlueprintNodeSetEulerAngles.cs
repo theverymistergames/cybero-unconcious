c900bef686b705ed96a2fae49624306683d00fe4
fotik333 <nobelisfm@mail.ru>
2023-05-12 11:51:39 +0300

    upd

---
+++ Assets/My/Blueprints/BlueprintNodeSetEulerAngles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MisterGames.Blueprints;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Set Euler Angles", Category = "Transform", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeSetEulerAngles : BlueprintNode, IBlueprintEnter {
+
+        [SerializeField] private Vector3 _angles;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter(),
+            Port.Input<Transform>("Transform"),
+            Port.Input<Vector3>("Rotation"),
+            Port.Exit(),
+        };
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            var transform = Ports[1].Get<Transform>();
+            var rotation = Ports[2].Get(_angles);
+
+            transform.eulerAngles = rotation;
+
+            Ports[3].Call();
+        }
+    }
+
+}
---
+++ Assets/My/Blueprints/BlueprintNodeTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MisterGames.Blueprints;
+using MisterGames.Character.Motion;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Time", Category = "Time", Color = BlueprintColors.Node.Data)]
+    public sealed class BlueprintNodeTime : BlueprintNode, IBlueprintOutput<float> {
+
+        private float _startTime = 0;
+        
+        public override Port[] CreatePorts() => new[] {
+            Port.Output<float>("Time"),
+            Port.Output<float>("Sine time"),
+            Port.Output<float>("Cosine time"),
+        };
+
+        public override void OnInitialize(IBlueprintHost host) {
+            base.OnInitialize(host);
+
+            _startTime = Time.time;
+        }
+
+        float IBlueprintOutput<float>.GetOutputPortValue(int port) => port switch {
+            0 => Time.time - _startTime,
+            1 => Mathf.Sin(Time.time - _startTime),
+            2 => Mathf.Cos(Time.time - _startTime) + 1,
+        };
+    }
+
+}
